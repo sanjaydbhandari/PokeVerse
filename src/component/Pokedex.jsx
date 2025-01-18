ec7375fcb0ab55d29bf6a9e4ed9cc956797d2af5
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2025-01-18 20:46:37 +0530

    update search box

--- src/component/Pokedex.jsx
+++ src/component/Pokedex.jsx
@@ -6,13 +6,13 @@ import { useDebounce } from "../hooks/useDebounce";
 const Pokedex = () => {
   const [searchItem, setSearchItem] = useState("");
 
-  const debouncedCall = useDebounce((e) => setSearchItem(e.target.value), 1000);
+  const debouncedCall = useDebounce((e) => setSearchItem(e.target.value.toLowerCase()), 1000);
 
   return (
     <div className="relative flex justify-center items-center w-screen flex-col space-y-5">
       {/* <h1 className="font-bold tracking-widest text-3xl mt-5">Pokemon</h1> */}
       <input
-        className="w-1/3 py-3 px-6 rounded-full bg-gray-500 outline-none"
+        className="md:w-1/3 py-3 px-6 rounded-full bg-gray-500 outline-none"
         placeholder="Search Pokemon Name..."
         type="search"
         name="search"
